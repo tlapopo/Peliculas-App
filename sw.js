3f79452ac1a4a739662e9865d7769f01f6b647ed
joel <tlapopoche@gmail.com>
2021-10-14 11:29:07 -0500

    Primeros cambios

---
+++ sw.js
@@ -0,0 +1,22 @@
+
+//INSTALACION DEL SERVICE WORKER
+self.addEventListener('install', () =>{
+ cvonsole.log('Service Worker install');
+});
+
+
+//ACTIVACION DE NUESTRO SERVICE WORKER
+self.addEventListener('activate', () => {
+    console.log('Service Worker activate');
+});
+
+//CACHE ONLY 
+self.addEventListener('fetch', event =>{
+    //VERIFICAMOS SI ES IGUAL ALGUN RECURSO EN CACHE
+    event.respondWith(caches.match(event.request));
+});
+
+//NETWORK ONLY
+self.addEventListener('fetch', ()=>{
+    event.respondWith(request);
+});
